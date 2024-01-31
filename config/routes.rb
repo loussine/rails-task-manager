fb8e9f8d0c2868a48609c3439dde32954e4617dd
Yolande Touchet <yolandetouchet@hotmail.com>
2024-01-31 20:20:58 +0100

    Rails 2 - CRUD - Rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,28 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # CREATE
+  # afficher le formulaire Task
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+  # http://127.0.0.1:3000/tasks/new
+
+  # post = générer l'envoi du formulaire
+  post 'tasks', to: "tasks#create"
+
+  # LISTER / INDEX
+  get 'tasks', to: 'tasks#index'
+
+  # FOCUS SUR UNE TASK
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # UPDATE UNE TASK
+  # afficher le formulaire de la Task à éditer
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  # edit GET  /tasks/:id/edit(.:format)              tasks#edit
+  # patch = insérer les modifications / update
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
